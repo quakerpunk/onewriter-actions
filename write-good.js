980163b6ac77e6932d41bb41c9c304811c3b7665
Shawn Borton <shawn@shawnborton.info>
2020-04-18 20:02:37 -0500

    Error checking

--- write-good.js
+++ write-good.js
@@ -5,9 +5,10 @@ const require = (moduleName) => {
     },
     (response, err) => {
       if (err) {
-        ui.alert(err.message);
+        let errorText = JSON.stringify(err);
+        ui.alert(errorText, "Error from require module.");
       }
-      if (response.responseText.startsWith("Cannot find package")) {
+      if (response && response.responseText.startsWith("Cannot find package")) {
         ui.alert(`Could not find ${moduleName} at unpkg.com`);
       }
     }
