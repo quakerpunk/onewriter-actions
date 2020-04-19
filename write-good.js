bf686c26558b63dc584790afe5c19905debf6b33
Shawn Borton <shawn@shawnborton.info>
2020-04-18 20:16:25 -0500

    Flesh Out Require Function

--- write-good.js
+++ write-good.js
@@ -1,17 +1,37 @@
+const debug = false;
 const require = (moduleName) => {
   http.request(
     {
       url: `https://unpkg.com/${moduleName}`,
     },
     (response, err) => {
-      if (err) {
-        ui.alert(`Could not download ${moduleName} from unpkg.com`);
+      if (err && err[0] == 404) {
+        ui.alert(`Could not find ${moduleName} at unpkg.com`);
       }
       if (response && response.responseText.startsWith("Cannot find package")) {
         ui.alert(`Could not find ${moduleName} at unpkg.com`);
       }
     }
   );
+
+  // Suppose the module tries to set module.exports.
+  // Well, we can't have that.
+  let module = {};
+  if (module.exports) return module.exports;
+
+  if (debug) {
+    if (response) {
+      ui.alert(JSON.stringify(response));
+    }
+    ui.alert(JSON.stringify(this));
+  }
 };
 
-let test1 = require("ufhgieei");
+// Make those NodeJS modules behave.
+const process = {
+  env: {
+    NODE_ENV: undefined,
+  },
+};
+
+let writeGood = require("write-good");
