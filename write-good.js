753094568271d05d905454c8599fd3ae96efb4de
Shawn Borton <shawn@shawnborton.info>
2020-04-18 19:55:57 -0500

    Error Message?

--- write-good.js
+++ write-good.js
@@ -3,7 +3,10 @@ const require = (moduleName) => {
     {
       url: `https://unpkg.com/${moduleName}`,
     },
-    (response) => {
+    (response, err) => {
+      if (err) {
+        ui.alert(err.message);
+      }
       if (response.responseText.startsWith("Cannot find package")) {
         ui.alert(`Could not find ${moduleName} at unpkg.com`);
       }
